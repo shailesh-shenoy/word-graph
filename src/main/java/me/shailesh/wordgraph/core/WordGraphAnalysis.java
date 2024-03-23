b39dc10ce817449accdbe53e98fb2d6602c4c6c3
shailesh-shenoy <shenoy.shailesh97@gmail.com>
2024-03-22 23:29:23 -0400

    Exploring graph implementations next

--- src/main/java/me/shailesh/wordgraph/core/WordGraphAnalysis.java
+++ src/main/java/me/shailesh/wordgraph/core/WordGraphAnalysis.java
@@ -1,6 +1,18 @@
 package me.shailesh.wordgraph.core;
 
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class WordGraphAnalysis {
+    private int n;
+    private Map<String, Integer> wordFrequencies;
+
+    public WordGraphAnalysis(String graph) {
+
+    }
     public static String buildGraph(String text) {
         return "graph";
     }
